91b2aa071d9bc206632d922850671036a0ccdcb6
raulgv <42993862+raulgv@users.noreply.github.com>
2018-10-02 17:45:12 +0200

    implemented multiply function

--- src/multiply.js
+++ src/multiply.js
@@ -1,3 +1,13 @@
-export const multiply = () => {
-  //
+export const multiply = (array) => {
+  let result = 1;
+  if (array.length === 0){
+    result = 0;
+    return result;
+  }
+  else if (array.length != 0) {
+    for(let i = 0; i < array.length;i++){
+      result *= array[i];
+    }
+    return result;
+  }
 };
